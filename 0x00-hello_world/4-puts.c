b662bf2dbcd859770a6cba82b3166b28083b1e0f
bakiizese <bereketzeselassie@gmail.com>
2023-02-16 01:57:40 -0800

    hello c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-
-\\this my first hello in shell
-
+/**
+ * main - Entry
+ * Return: Always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
