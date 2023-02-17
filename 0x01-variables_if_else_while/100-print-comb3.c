d7d9dd88fdcc27bbf12b73d894ebb2b77bc4f995
bakiizese <bereketzeselassie@gmail.com>
2023-02-17 07:16:43 -0800

    complex

---
+++ 0x01-variables_if_else_while/100-print-comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	int c;
+
+	while (i < 10)
+	{
+		c = 0;
+
+		while (c < 10)
+		{
+			if (i != c && i < c)
+			{
+				putchar('0' + i);
+				putchar('0' + c);
+
+				if (c + i != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			i++;
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
