f08078e2c0f3850520bcb4f7ba0adef68864c4d5
bakiizese <bereketzeselassie@gmail.com>
2023-02-17 06:39:16 -0800

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,13 @@ int main(void)
 {
 	int num = 0;
 
-	while (num <= 9)
+	for ( ; num <= 9; num++)
 	{
-		putchar(num+'0');
+		putchar(num + '0');
+		if (num == 9)
+			break;
 		putchar(',');
 		putchar(' ');
-		++num;
 	}
 	putchar('\n');
 	return (0);
