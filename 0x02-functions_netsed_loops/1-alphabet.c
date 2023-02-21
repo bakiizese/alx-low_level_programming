08c532bf910eb9363766403c3985048bd0563563
bakiizese <bereketzeselassie@gmail.com>
2023-02-21 01:27:36 -0800

    alpha_x10

--- 0x02-functions_netsed_loops/1-alphabet.c
+++ 0x02-functions_netsed_loops/1-alphabet.c
@@ -1,18 +1,7 @@
-#include "stdio.h"
-/**
- * main - Entry
- * Return: 0
- */
-
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return(0);
-}
+#include "main.h"
 /**
  * print_alphabet - void
+ * return: 0
  */
 void print_alphabet(void)
 {
@@ -20,7 +9,7 @@ void print_alphabet(void)
 
 	for ( ; chr <= 'z'; chr++)
 	{
-		putchar(chr);
+		_putchar(chr);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
