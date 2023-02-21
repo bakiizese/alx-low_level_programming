9e0f91dc1c77df24c4fa2a463049b974b7f102b7
bakiizese <bereketzeselassie@gmail.com>
2023-02-21 05:17:34 -0800

    8-24

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,7 @@ int print_sign(int n);
 int _isalpha(int c);
 int _abs(int n);
 int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int i, int j);
+void print_to_98(int n);
