d4f0ac67e3565ec4cbd9c32b6348c4a99d540a6c
bakiizese <bereketzeselassie@gmail.com>
2023-02-21 04:40:53 -0800

    10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- * print_alphabet_x10(void)
+ * print_alphabet_x10 - prints 10 times
  */
 
 void print_alphabet_x10(void)
 {
 	int count = 0;
 
-	while (count < 9)
+	while (count < 10)
 	{
 		char chr = 'a';
+
 		for ( ; chr <= 'z'; chr++)
 		{
 			_putchar(chr);
-		}			
+		}
 		_putchar('\n');
-		count++;	
+		count++;
 	}
 }
