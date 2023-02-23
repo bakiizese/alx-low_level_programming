6a355ace860de378b040b8b399c683a7a1e9b858
bakiizese <bereketzeselassie@gmail.com>
2023-02-23 07:12:26 -0800

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,7 +11,7 @@ void print_triangle(int size)
 	{
 		for (i = 0; i <= (size - 1); i++)
 		{
-			for (j = 0; j <= (size - 1) - i; j++)
+			for (j = 0; j < (size - 1) - i; j++)
 			{
 				_putchar(' ');
 			}
