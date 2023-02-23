17f1d6d0e1c5d3af8b304ddc95eb3146ad73a47e
bakiizese <bereketzeselassie@gmail.com>
2023-02-23 07:06:43 -0800

    traingle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,19 +1,22 @@
 #include "main.h"
-
+/**
+ * print_triangle - entry
+ * @size: size
+ */
 void print_triangle(int size)
 {
 	int i, j, k;
+
 	if (size > 0)
 	{
-		for (i = 0; i <= (size-1); i++)
+		for (i = 0; i <= (size - 1); i++)
 		{
-			for (j = 0; j <= (size-1) - i; j++)
+			for (j = 0; j <= (size - 1) - i; j++)
 			{
 				_putchar(' ');
 			}
 			for (k = 0; k <= i; k++)
 				_putchar('#');
-			
 			_putchar('\n');
 		}
 	}
