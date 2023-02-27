36466880e86c54a4b4cfadeb9fae005e577364d9
bakiizese <bereketzeselassie@gmail.com>
2023-02-27 01:05:01 -0800

    half puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - entry
+ * @str: var
+ */
+void puts_half(char *str)
+{
+	int i, j;
+	int st = strlen(str);
+
+	if (st % 2 == 0)
+	{
+		i = st / 2;
+		for ( ; i <= st; i++)
+		{
+			j = str[i];
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	else if (st % 2 != 0)
+	{
+		i = (st - 1) / 2;
+		for ( ; i <= st; i++)
+		{
+			j = str[i];
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
