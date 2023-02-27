ef81636c75abeffa3824dd2a7d3c04a23528e837
bakiizese <bereketzeselassie@gmail.com>
2023-02-27 01:58:07 -0800

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,9 +9,10 @@ void puts_half(char *str)
 	int i, n;
 	int len = 0;
 
-	while(str[len] != '\0')
+	while(str[len] != '\0');
+	{
 		len++;
-
+	}
 	if (len % 2 == 0)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
