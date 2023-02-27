ea4d4465fa729b9005128bd3e62a7e82b5518e90
bakiizese <bereketzeselassie@gmail.com>
2023-02-27 05:52:41 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,5 +6,28 @@
  */
 int _atoi(char *s)
 {
-	return (*s);
+	int i = 0, n = 0;
+	int sign = 0;
+
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
+	{
+		if ( n >= 0)
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+		else
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+	}
+	sign *= -1;
+	return (n * sign);
 }
