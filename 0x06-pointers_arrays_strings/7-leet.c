7aac059171c162b33cbd8bc0320a2bfc91eb71f9
bakiizese <bereketzeselassie@gmail.com>
2023-03-01 06:40:27 -0800

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * leet - entry
+ * @str: var
+ * Return: str
+ */
+char *leet(char *str)
+{
+	int i = 0, j;
+	char let[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 't'};
+
+	while (str[i])
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			if (str[i] == let[j] || str[i] - 32 == let[j])
+				str[i] = j + '0';
+		}
+		i++;
+	}
+	return (str);
+}
