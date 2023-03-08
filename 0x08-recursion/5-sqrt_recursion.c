d89ebc45af8a1488d6c8aea71eace4f966f26c2f
bakiizese <bereketzeselassie@gmail.com>
2023-03-08 04:23:46 -0800

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+int sq(int n, int i);
+
 int _sqrt_recursion(int n)
 {
-	else if (n > 0)
-		return (n / _sqrt_recursion(n / l));
-	else if (n %
+	return (sq(n, 1));
+}
+int sq(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i * i < n)
+		return (sq(n, i + 1));
 	else
-		return (1);
+		return (-1);
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,19 +2,5 @@
 
 int is_prime_number(int n)
 {
-	int i = n/2;
-
-	if (i == 1)
-		return (1);
-	else
-	{
-		if (n % i == 0)
-		{
-			return 0;
-		}
-		else
-		{
-			return (1);
-		}
-	}
+	return (n/2);
 }
