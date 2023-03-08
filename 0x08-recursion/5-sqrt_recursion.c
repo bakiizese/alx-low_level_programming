7195c2df7a31734e8c07cca358c23ec5602c52c7
bakiizese <bereketzeselassie@gmail.com>
2023-03-08 03:44:04 -0800

    rec

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int _sqrt_recursion(int n)
+{
+	else if (n > 0)
+		return (n / _sqrt_recursion(n / l));
+	else if (n %
+	else
+		return (1);
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int is_prime_number(int n)
+{
+	int i = n/2;
+
+	if (i == 1)
+		return (1);
+	else
+	{
+		if (n % i == 0)
+		{
+			return 0;
+		}
+		else
+		{
+			return (1);
+		}
+	}
+}
