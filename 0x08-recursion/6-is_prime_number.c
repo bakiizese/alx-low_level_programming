43c9b6f28211ab756f4621f68ded688c80b7c579
bakiizese <bereketzeselassie@gmail.com>
2023-03-08 05:19:12 -0800

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -25,12 +25,9 @@ int is_prime_number(int n)
 int prime(int n, int i)
 {
 	if (i == 1)
-		return(1);
+		return (1);
+	else if (n % i == 0)
+		return (0);
 	else
-	{
-		if (n % i == 0)
-			return (0);
-		else
-			return (prime(n, i - 1));
-	}
+		return (prime(n, i - 1));
 }
