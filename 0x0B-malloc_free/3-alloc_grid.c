afb91dfa161a56d06e5ca9b3169bdd4f3d6854c8
bakiizese <bereketzeselassie@gmail.com>
2023-03-13 09:00:58 -0700

    fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(width * sizeof(int *));
+		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
 			for (i -= 1; i >= 0; i--)
