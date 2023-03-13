796a861ab1f8a006d398330c41ba68e68fdb29ea
bakiizese <bereketzeselassie@gmail.com>
2023-03-13 00:54:46 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	char *ptr = (char *)malloc(size * sizeof(char));
+	if (size == 0)
+		return (NULL);
+	if (ptr == NULL)
+		return (0);
+	for (i = 0; i < size; i++)
+	{
+		*(ptr + i) = c;
+	}
+	return ptr;
+}
