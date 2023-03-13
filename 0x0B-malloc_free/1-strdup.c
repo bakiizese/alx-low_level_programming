49e45d0abc56e2cc0f57ea6f7fe5a09b97b80be5
bakiizese <bereketzeselassie@gmail.com>
2023-03-13 01:52:08 -0700

    dup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,21 +16,35 @@ int count(char *i)
 	return (j);
 }
 /**
+ * _strcpy - entry
+ * @ptr: var
+ * @str: var
+ * Return: ptr
+ */
+char *_strcpy(char *ptr, char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+/**
  * _strdup - entry
  * @str: var
  * Return: ptr
  */
 char *_strdup(char *str)
 {
-	int c = count(str);
+	int c = count(str) + 1;
 	char *ptr = (char *)malloc(c * sizeof(char));
-	int i;
 
-	if (str == NULL)
-		return (1);
-	for (i = 0; i <= c; i++)
-	{
-		*(ptr + i) = str[i];
-	}
+	if (ptr == NULL)
+		return (NULL);
+	_strcpy(ptr, str);
 	return (ptr);
 }
