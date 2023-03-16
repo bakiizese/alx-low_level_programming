e9345fe3a766b75b43403b0a76eec4da4b7bd104
bakiizese <bereketzeselassie@gmail.com>
2023-03-16 06:25:35 -0700

    mul

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: var
+ * @argv: var
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	unsigned long mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%ld\n", mul);
+	return (0);
+}
