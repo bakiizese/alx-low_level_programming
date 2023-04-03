4f9be6a391ae631dfada000cd34b2b21e09238da
bakiizese <bereketzeselassie@gmail.com>
2023-04-03 02:35:05 -0700

    add_nodeint

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * add_nodeint - entry
+ * @head: pointer to pointer head
+ * @n: var
+ * Return: pointer head
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;	
+	return (*head);
+}
