978f5b1ebebb24fd114bd3c8d339da779a868f0e
bakiizese <bereketzeselassie@gmail.com>
2023-04-04 08:31:50 -0700

    sum of all lists

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_listint - entry
+ * @head: pointer to head
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
