0c6542b0a707574b1c3f22cb5e607db69f885c72
bakiizese <bereketzeselassie@gmail.com>
2023-04-06 05:50:36 -0700

    fixed

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,9 +7,16 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index  >= (sizeof(unsigned long int) * 0))
-		return (-1);
-	if ((n & (1 << index)) == 0)
+	unsigned int i = 0;
+
+	if (index  < 64 && n == 0)
 		return (0);
-	return (1);
+	for (; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+	return (-1);
 }
