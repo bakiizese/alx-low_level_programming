b70bf2fe053121b4f6b01de184269fb77247ac69
bakiizese <bereketzeselassie@gmail.com>
2023-04-10 05:36:16 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,10 +11,27 @@ void closed(int cs)
 
 	if (i == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", opf);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cs);
 		exit(100);
 	}
 }
+/**
+ * create - entry
+ * @f: var
+ * Return: b
+ */
+char *create(char *f)
+{
+	char *b = malloc(sizeof(char) * 1024);
+
+	if (b == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f);
+		exit(99);
+	}
+	return (b);
+}
+
 /**
  * main - entry
  * @argc: number of agrs
@@ -24,24 +41,19 @@ void closed(int cs)
 int main(int argc, char *argv[])
 {
 	int re, wr, opt, opf;
-	char *fp = malloc(sizeof(char) * 1024);
+	char *fp;
 
-	if (fp == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	fp = create(argv[2]);
 	opf = open(argv[1], O_RDONLY);
 	re = read(opf, fp, 1024);
 	opt = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	while (re >= 0)
-	{
+	do {
 		if (opf == -1 || re == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
@@ -57,7 +69,7 @@ int main(int argc, char *argv[])
 		}
 		re = read(opf, fp, 1024);
 		opt = open(argv[2], O_WRONLY | O_APPEND);
-	}
+	} while (re > 0);
 	free(fp);
 	closed(opf);
 	closed(opt);
