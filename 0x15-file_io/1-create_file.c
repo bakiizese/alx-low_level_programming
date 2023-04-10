7ac4058f935cb1aeb202ce7c2199316e2c78e7bf
bakiizese <bereketzeselassie@gmail.com>
2023-04-10 03:21:01 -0700

    creating file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_file - entry
+ * @filename: file
+ * @text_content: buf
+ * Return: 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int op, wr;
+	int count  = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[count] != '\0')
+			count++;
+	}
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	wr = write(op, text_content, count);
+	if (op == -1 || wr == -1)
+		return (-1);
+	close(op);
+	return (1);
+}
