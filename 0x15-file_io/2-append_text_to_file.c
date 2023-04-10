5b9f925abf3fbcd45456bca1fc6005e747257051
bakiizese <bereketzeselassie@gmail.com>
2023-04-10 04:24:23 -0700

    fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int count = 0;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 	op = open(filename, O_WRONLY | O_APPEND);
 	if (op == -1)
 	{
