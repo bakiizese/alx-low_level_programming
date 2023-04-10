e3d8c22573d10ff9c92d5eb4b83d531f0fd59978
bakiizese <bereketzeselassie@gmail.com>
2023-04-10 04:21:46 -0700

    appending

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * append_text_to_file - entry
+ * @filename: file
+ * @text_content: text
+ * Return: 1
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int op;
@@ -10,7 +15,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	op = open(filename, O_WRONLY | O_APPEND);
 	if (op == -1)
 	{
-		close(op);
 		return (-1);
 	}
 	if (text_content != NULL)
@@ -26,4 +30,3 @@ int append_text_to_file(const char *filename, char *text_content)
 	close(op);
 	return (1);
 }
-	
