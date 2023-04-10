a9d636ac16ff193d6c28a09d838f52556f3cc898
bakiizese <bereketzeselassie@gmail.com>
2023-04-10 03:59:09 -0700

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		while (text_content[count] != '\0')
+		while (text_content[count])
 			count++;
 	}
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
