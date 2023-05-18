2c3e0471799cb9166d8737d4747b94ce92db2f58
bakiizese <bereketzeselassie@gmail.com>
2023-05-18 05:11:46 -0700

    inserts

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,15 +1,48 @@
 #include "lists.h"
+/**
+ * insert_dnodeint_at_index - entry
+ * @h: struct var
+ * @idx: index
+ * @n: var
+ * Return: new
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t));
-	unsigned int count = 0;
+	dlistint_t *head, *new = NULL;
+	unsigned int count;
 
-	if (new == NULL)
-		return (NULL);
-	while (head->prev != NULL)
-		head = head->prev;
-	while (head != NULL)
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
 	{
-		if (count = index)
+		head = *h;
+		count = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
 		{
-
+			if (count == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			count++;
+		}
+	}
+	return (new);
+}
