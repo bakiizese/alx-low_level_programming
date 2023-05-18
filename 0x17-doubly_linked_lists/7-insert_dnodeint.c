3837325b7a42dba01405d8e6251885350d633206
bakiizese <bereketzeselassie@gmail.com>
2023-05-18 05:20:29 -0700

    fixed

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,6 +33,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 						head->next->prev = new;
 						head->next = new;
 					}
+					else
+						return (NULL)
 				}
 				else
 					new = add_dnodeint_end(h, n);
