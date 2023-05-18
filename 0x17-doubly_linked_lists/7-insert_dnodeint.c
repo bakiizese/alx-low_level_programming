39a9990c0e02b4b832a3dcd40ab589223ed226f6
bakiizese <bereketzeselassie@gmail.com>
2023-05-18 01:25:11 -0700

    at indexs

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	unsigned int count = 0;
+
+	if (new == NULL)
+		return (NULL);
+	while (head->prev != NULL)
+		head = head->prev;
+	while (head != NULL)
+	{
+		if (count = index)
+		{
+
