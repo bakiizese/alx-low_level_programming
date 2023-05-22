333568d04c550c0e4e8a6f2d3115d194a11eca7d
bakiizese <bereketzeselassie@gmail.com>
2023-05-22 00:53:19 -0700

    dynamic libs

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - entry
+ * @str: var
+ */
+void _puts(char *str)
+{
+	int i;
+	int j;
+	int st = strlen(str);
+
+	for (i = 0; i <= st - 1; i++)
+	{
+		j = str[i];
+		_putchar(j);
+	}
+	_putchar('\n');
+}
