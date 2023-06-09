cba4027ff840edba6bdf648d264682d06d0c2622
bakiizese <bereketzeselassie@gmail.com>
2023-06-09 14:25:01 -0700

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,10 +6,11 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *t = malloc(sizeof(hash_table_t));
+	hash_table_t *t;
 	hash_node_t **ar;
 	unsigned long int j;
 
+	t = malloc(sizeof(hash_table_t));
 	if (t == NULL)
 		return (NULL);
 	ar = malloc(sizeof(hash_node_t *) * size);
