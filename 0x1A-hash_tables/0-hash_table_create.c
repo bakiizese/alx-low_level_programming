2bda9c5919f5abb966ad70dd07ea58a9a47ce9a6
bakiizese <bereketzeselassie@gmail.com>
2023-06-09 14:19:42 -0700

    hash_tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - entry
+ * @size: var
+ * Return: t
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *t = malloc(sizeof(hash_table_t));
+	hash_node_t **ar;
+	unsigned long int j;
+
+	if (t == NULL)
+		return (NULL);
+	ar = malloc(sizeof(hash_node_t *) * size);
+	if (ar == NULL)
+		return (NULL);
+	j = 0;
+	while (j < size)
+	{
+		ar[j] = NULL;
+		j++;
+	}
+	t->array = ar;
+	t->size = size;
+	return (t);
+}
