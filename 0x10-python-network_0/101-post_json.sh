5c67e1b05b9e041d04245bd1a6f6a937e8cd5141
bakiizese <bereketzeselassie@gmail.com>
2024-01-05 08:25:12 -0800

    fix

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#json
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# catvh
+curl -sL -X PUT -H "You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
