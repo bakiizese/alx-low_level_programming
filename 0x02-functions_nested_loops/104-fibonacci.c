bd8a2a9526d9986a51bd4e502696155969774ca7
bakiizese <bereketzeselassie@gmail.com>
2024-01-13 20:48:25 -0800

    fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 /**
- * main - Entry
+ * fibp - entry
+ * @n: var
+ */
+void fibp(int n)
+{
+	int a = 1, b = 2, i = 3;
+
+	printf("%d, %d", a, b);
+
+	for (i = 3; i <= n; ++i)
+	{
+		int next = a + b;
+
+		printf(", %d", next);
+		a = b;
+		b = next;
+	}
+}
+/**
+ * main - entry
  * Return: 0
  */
 int main(void)
 {
+	int n = 98;
+
+	fibp(n);
+	printf("\n");
+
 	return (0);
 }
