65fc3f01e20a9bbe462b7797655f1f4563e8be11
bakiizese <bereketzeselassie@gmail.com>
2024-01-16 10:40:04 -0800

    fix ;]

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -37,7 +37,7 @@ int jump_search(int *array, size_t size, int value)
 				if (array[beg] == value)
 				{
 					for (; val <= beg; val++)
-						printf("Value checked array [%ld] = [%d]\n", val, array[val]);
+						printf("Value checked array[%ld] = [%d]\n", val, array[val]);
 					return (beg);
 				}
 			}
