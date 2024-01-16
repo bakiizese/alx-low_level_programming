b40050b509600aa880618b96b87fac4c2d115ba0
bakiizese <bereketzeselassie@gmail.com>
2024-01-16 13:16:48 -0800

    interp

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -33,6 +33,11 @@ int interpolation_search(int *array, size_t size, int value)
 			pos = low + (((double)(high - low) / (array[high] - array[low])) *
 					(value - array[low]));
 		}
+		else
+		{
+			printf("Value checked array[%ld] is out of range\n", pos);
+			return (-1);
+		}
 	}
 	printf("Value checked array[%ld] is out of range\n", pos);
 	return (-1);
