267f7a477eff4ec748318061de4e4306cfbe2a9f
bakiizese <bereketzeselassie@gmail.com>
2024-01-16 07:53:50 -0800

    fix

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -30,7 +30,7 @@ int jump_search(int *array, size_t size, int value)
 			mid += m;
 			if (i > end)
 			{
-				printf("Value found between indexes[%ld] and [%ld]\n", end, i);
+				printf("Value found between indexes [%ld] and [%ld]\n", end, i);
 				printf("Value checked array[%ld] = [%d]\n", end, array[end]);
 				return (-1);
 			}
