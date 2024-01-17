8022649a8ac9e181245c8560c9e88038d2b83975
bakiizese <bereketzeselassie@gmail.com>
2024-01-17 11:35:20 -0800

    fix

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,44 +1,73 @@
 #include "search_algos.h"
 #include <math.h>
-
+int ret(int *array, size_t end, size_t i);
 /**
- * jump_search - searches for a value in an array of
- * integers using the Jump search algorithm
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * jump_search - entry
+ * @array: array
+ * @size: num of array
+ * @value: num to be searched
+ * Return: index of value
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int index, m, k, prev;
+	size_t m = sqrt(size);
+	size_t beg = 0;
+	size_t end = size - 1;
+	size_t mid = m;
+	size_t val, i;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	m = (int)sqrt((double)size);
-	k = 0;
-	prev = index = 0;
-
-	do {
-		printf("Value checked array[%d] = [%d]\n", index, array[index]);
-
-		if (array[index] == value)
-			return (index);
-		k++;
-		prev = index;
-		index = k * m;
-	} while (index < (int)size && array[index] < value);
-
-	printf("Value found between indexes [%d] and [%d]\n", prev, index);
-
-	for (; prev <= index && prev < (int)size; prev++)
+	while (end >= beg)
 	{
-		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
-		if (array[prev] == value)
-			return (prev);
+		printf("Value checked array[%ld] = [%d]\n", beg, array[beg]);
+		i = mid;
+		if (array[beg] == value)
+			return (beg);
+		if (array[mid] < value)
+		{
+			beg = mid;
+			mid += m;
+			if (i > end)
+				return (ret(array, end, i));
+		}
+		else if (array[mid] >= value)
+		{
+			end = mid;
+			printf("Value found between indexes [%ld] and [%ld]\n", beg, end);
+			if (value < 0)
+			{
+				for (; beg <= end; beg++)
+				{
+					printf("Value checked array[%ld] = [%d]\n", beg, array[beg]);
+				}
+				return (-1);
+			}
+			val = beg;
+			for (; beg <= end; beg++)
+			{
+				if (array[beg] == value)
+				{
+					for (; val <= beg; val++)
+						printf("Value checked array[%ld] = [%d]\n", val, array[val]);
+					return (beg);
+				}
+			}
+		}
 	}
-
+	return (-1);
+}
+/**
+ * ret - entry
+ * @array: array
+ * @end: end of array index
+ * @i: mid of array
+ * Return: -1
+ */
+int ret(int *array, size_t end, size_t i)
+{
+	printf("Value found between indexes [%ld] and [%ld]\n", end, i);
+	printf("Value checked array[%ld] = [%d]\n", end, array[end]);
 	return (-1);
 }
